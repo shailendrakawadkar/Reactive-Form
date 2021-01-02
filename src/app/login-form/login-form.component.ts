91627b78a23dfec2e9123a98908f8b7333bbe7ca
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2021-01-02 16:40:58 +0530

    login-form
    Reactive login form done.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,32 +1,32 @@
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms'; 
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
- 
+
 })
 
- 
+
 
 export class LoginFormComponent implements OnInit {
- 
-  loginForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    
-    this.loginForm = fb.group({
-     
-     user_name : ['',[ Validators.required,Validators.email]],
-      userPass : ['', Validators.required]
-    })
- 
-  }
 
+  constructor(private fb: FormBuilder) {  }
+ 
+get userPass() {
+  return this.loginForm.get("userPass");
+}
+
+  loginForm = this.fb.group({
+
+    user_name: ['', [Validators.required, Validators.email]],
+    userPass: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
+  })
   ngOnInit(): void {
- 
 
-  } 
+
+  }
 }
